26dd48e8430fb31c8476859c92147ac19c41c15d
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-07-31 08:15:37 -0600

    Starting Auth.

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const massive = require('massive');
+const axios = require('axios');
+
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false
+}))
+
+app.get('auth/callback', async (req, res) => {
+
+    const payload = {
+        client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+        client_secret: process.env.AUTH0_CLIENT_SECRET,
+        code: req.query.code,
+        grant_type: 'authorization_code',
+        redirect_uri: `http://${req.headers.host}/auth/callback`
+    };
+//----get token---//
+
+    let receiveToken = await axios.post(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/oauth/token`, payload)
+
+//----exchange token---//
+
+let receiveUser = await axios.post(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/userinfo/?access_token=${receiveToken.data.access_token}`)
+
+//-----user data-----//
+req.session.user = receiveUser.data;
+console.log(receiveUser.data)
+res.redirect('/texteditor');
+
+})
+
+app.post('/api/logout', (req, res) => {
+    req.session.destroy();
+    res.send();
+})
+
+app.get('/api/user-data', (req, res) => {
+    res.json({user: req.session.user})
+})
+
+
+
+const SERVER_PORT = process.env.SERVER_PORT || 3040;
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening: ${SERVER_PORT}`)
+})
