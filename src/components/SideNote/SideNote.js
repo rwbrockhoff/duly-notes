daa1004fe7d5d1d6c941fe10c11c7f1f000d187d
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-07-31 19:39:09 -0600

    Displaying notes semi-functional.

---
+++ src/components/SideNote/SideNote.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './SideNote.css';
+
+export default class SideNote extends Component {
+  render(props) {
+      console.log('note', this.props.note)
+      console.log('index', this.props.index)
+    return (
+      <div className='container'>
+        <p className='title'> {this.props.note.title}</p>
+        <p className='preview'>{this.props.note.content)}</p>
+      </div>
+    )
+  }
+}
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
 import './Sidebar.css';
+import {connect} from 'react-redux';
+import SideNote from '../SideNote/SideNote';
+let listOutNotes = null;
 
-export default class Sidebar extends Component {
+
+class Sidebar extends Component {
+  
   render() {
+    if (!this.props.notes){
+      listOutNotes = null;
+      console.log('nothing here, mate');
+    }
+    else {
+
+      listOutNotes = this.props.notes.map((e,i) => {
+        return (
+        <SideNote note={e} index={i}/>
+        )
+    })
+        
+  }
+    
     return (
       <div className='sidebar'>
             <div className="iconbar">
                 <p>Icons</p>
             </div>
+
+            <div className="noteContainer">
+             {listOutNotes}
+            </div>
             
-
-
-
-
-
       </div>
     )
   }
+
+  }
+
+function mapStateToProps(state){
+  return {
+    ...this.props, ...state
+  }
 }
+
+export default connect(mapStateToProps)(Sidebar)
