cd64f41bff125bb29724f5a069be04726022c215
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-08-04 23:47:29 -0600

    Contact form built. SendGrid set up in backend.

--- server/index.js
+++ server/index.js
@@ -130,6 +130,19 @@ const {email, name, sub} = req.session.user
 //--------STRIPE-------------//
 
 
+//---------SENDGRID--------//
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+    to: 'brockhoffrw@gmail.com',
+    from: 'admin@sidenote.com',
+    subject: 'Sending WITHOUT OR with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+//   sgMail.send(msg);
+
+//--------------------------//
 
 const SERVER_PORT = process.env.SERVER_PORT || 3040;
 app.listen(SERVER_PORT, () => {
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,15 +9,23 @@ export default class Contact extends Component {
       <div className="textinfo">
       <h1>Questions? Comments? ...Opinion on the 5th season of Friends?</h1>
       <div className='response'>
-      <h3>Fill out this form. We'll read it over</h3>
-      <h1> ☕</h1> 
-      <h3> in the next 1-3 business days.    </h3>
+      <h2>Fill out this form. We'll read it over ☕ in the next 1-2 business days.</h2>
+      
       </div>
 
 
       </div>
 
      <div className="form">
+    
+        <input placeholder={"Name"}/>
+      
+        <input type={"email"} placeholder={"name@company.com"}/>
+      
+        <textarea placeholder={"Message"}/>
+
+        <button>Submit</button>
+
      </div>
 
 
