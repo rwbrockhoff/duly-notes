609c80fe397072b5bca960840f5641b9a41e5bee
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-08-17 13:41:12 -0600

    Remove pomodoro from preferences menu.

--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -354,25 +354,7 @@ class TextEditor extends Component {
                       />
                   </li> 
 
-                   <li> 
-                    <i className="fas fa-moon"/> &nbsp; Pomodoro &nbsp;
-                    <Switch
-                        checked={this.state.checkedPomodoro}
-                        onChange={this.handlePomodoroChange}
-                        onColor="#86d3ff"
-                        onHandleColor="#2693e6"
-                        handleDiameter={15}
-                        uncheckedIcon={false}
-                        checkedIcon={false}
-                        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-                        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-                        height={10}
-                        width={24}
-                        className="react-switch"
-                        id="material-switch"
-                      />
-                  </li> 
-                
+                  
                 <li onClick={() => this.logout()}> 
                   <Link to ='/'> 
                   <i className="fas fa-sign-out-alt"/> &nbsp; Logout </Link></li> 
