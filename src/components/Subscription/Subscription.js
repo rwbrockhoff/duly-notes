b370fee0fb96206c3344dccfcc104413647371e0
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-08-20 01:03:22 -0600

    Add Loading... to State.Name in Sub.

--- src/components/Subscription/Subscription.js
+++ src/components/Subscription/Subscription.js
@@ -14,7 +14,7 @@ class Subscription extends Component {
 constructor(){
     super()
     this.state = {
-        name: '',
+        name: 'Loading...',
         startDate: '',
         status: '', 
         paymentInfo: '',
