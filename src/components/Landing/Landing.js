dc20e4483dac97991f27ec1500f01f21691e2574
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-01-17 20:11:05 -0600

    Font Family issues resolved. CSS cleaned up.

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,44 +1,43 @@
 import React, { Component } from 'react'
 import './Landing.css';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import UserIcon from '../UserIcon/UserIcon'
 
 class Landing extends Component {
-  
-  login(){
-    let {REACT_APP_AUTH0_DOMAIN,
-    REACT_APP_AUTH0_CLIENT_ID
-  } = process.env;
 
-  let url = `${encodeURIComponent(window.location.origin)}/auth/callback`
+  login() {
+    let { REACT_APP_AUTH0_DOMAIN,
+      REACT_APP_AUTH0_CLIENT_ID
+    } = process.env;
 
-  window.location = `https://${REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_url=${url}&response_type=code`
-}
+    let url = `${encodeURIComponent(window.location.origin)}/auth/callback`
+
+    window.location = `https://${REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_url=${url}&response_type=code`
+  }
 
   render(props) {
-    let loginUI = this.props.name ? 
-    <UserIcon {...props}/>
-    : 
-    <li className='login' onClick={this.login}>login</li>
-   
+    let loginUI = this.props.name ?
+      <UserIcon {...props} />
+      :
+      <li className='login' onClick={this.login}>login</li>
+
     return (
       <div className='landing'>
-        
-        <div className='header-bar center'>
-            <div className='logo center'><Link to="/"> duly notes </Link></div>
-            <nav>
-                <Link to="/contact"><li className="landinglinks">mindset</li></Link>
-                <Link to="/contact"><li className="landinglinks">contact</li></Link>
-               {loginUI}
-            </nav>
+
+        <div className='header-bar flex-center'>
+          <div className='logo flex-center'><Link to="/"> duly notes </Link></div>
+          <nav>
+            <Link to="/contact"><li className="landinglinks">contact</li></Link>
+            {loginUI}
+          </nav>
         </div>
-      
+
       </div>
     )
   }
 }
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
     ...this.props, ...state
   }
--- src/components/LandingFeatures/LandingFeatures.js
+++ src/components/LandingFeatures/LandingFeatures.js
@@ -4,73 +4,73 @@ import HeaderImage from '../../assets/Header.png'
 
 
 export default class LandingFeatures extends Component {
-  register(){
-    let {REACT_APP_AUTH0_DOMAIN,
-    REACT_APP_AUTH0_CLIENT_ID
-  } = process.env;
+  register() {
+    let { REACT_APP_AUTH0_DOMAIN,
+      REACT_APP_AUTH0_CLIENT_ID
+    } = process.env;
 
-  let url = `${encodeURIComponent(window.location.origin)}/auth/callback`
+    let url = `${encodeURIComponent(window.location.origin)}/auth/callback`
 
-  window.location = `https://${REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_url=${url}&response_type=code`
-}
+    window.location = `https://${REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_url=${url}&response_type=code`
+  }
 
-    render() {
-      
+  render() {
+
 
     return (
       <React.Fragment>
 
-        <div className='intro center column'>
-        <img src={HeaderImage} alt='computerdemo'/>
-        <h1> du more with duly</h1>
-        <h5>productive notes for productive people </h5>
-        <button onClick={this.register}>sign up </button>
+        <div className='intro flex-center column'>
+          <img src={HeaderImage} alt='computerdemo' />
+          <h1> du more with duly</h1>
+          <h5>productive notes for productive people </h5>
+          <button onClick={this.register}>sign up </button>
         </div>
 
         <div className='features flex column'>
-            <h1> Features </h1>
-            <div className='grid'>
-           <div className='grid-item center column'>
-           <i className="fas fa-outdent"/>
-            <h2>Inline Editing</h2>
-            <p>No more toolbar clutter. Focus more on writing.</p>
-            </div>
-            
-             <div className='grid-item center column'>
-             <i className="fab fa-markdown"/>
-            <h2>Markdown</h2>
-            <p>The markdown you're used to, updating as you type. </p>
+          <h1> Features </h1>
+          <div className='grid'>
+            <div className='grid-item flex-center column'>
+              <i className="fas fa-outdent" />
+              <h2>Inline Editing</h2>
+              <p>No more toolbar clutter. Focus more on writing.</p>
             </div>
 
-             <div className='grid-item center column'>
-             <i className="fas fa-stopwatch"/>
-            <h2>Pomodoro</h2>
-            <p>One click away from starting  a pomodoro. Stay on task. </p>
+            <div className='grid-item flex-center column'>
+              <i className="fab fa-markdown" />
+              <h2>Markdown</h2>
+              <p>The markdown you're used to, updating as you type. </p>
             </div>
 
-             <div className='grid-item center column'>
-             <i className="fas fa-brain"/>
-            <h2>Memory Gradient</h2>
-            <p>The science of learning has a color scheme. </p>
+            <div className='grid-item flex-center column'>
+              <i className="fas fa-stopwatch" />
+              <h2>Pomodoro</h2>
+              <p>One click away from starting  a pomodoro. Stay on task. </p>
             </div>
 
-            <div className='grid-item center column'>
-            <i className="fas fa-moon"/>
-            <h2>Dark Theme</h2>
-            <p>One click and you're in a different world. </p>
+            <div className='grid-item flex-center column'>
+              <i className="fas fa-brain" />
+              <h2>Memory Gradient</h2>
+              <p>The science of learning has a color scheme. </p>
+            </div>
+
+            <div className='grid-item flex-center column'>
+              <i className="fas fa-moon" />
+              <h2>Dark Theme</h2>
+              <p>One click and you're in a different world. </p>
             </div>
 
 
-            </div>
+          </div>
 
-        
+
         </div>
-           
 
-            <footer className="center"> 
-              <h2>duly</h2> &nbsp;&nbsp;<h2>|</h2> &nbsp; www.dulynotes.com
+
+        <footer className="flex-center">
+          <h2>duly notes</h2> &nbsp;&nbsp;<h2>|</h2> &nbsp; www.dulynotes.com
             </footer>
-    </React.Fragment>
+      </React.Fragment>
     )
   }
 }
